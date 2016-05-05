c6c7f5d96f9240fd2f0c3c319aa5e97d3b7045a9
chenbin <c1325791024@163.com>
2016-05-05 23:50:36 +0800

    add hash testcase

--- src/test/java/com/jingoal/web/redis/sample/test/Redis4RedisTemplateTest.java
+++ src/test/java/com/jingoal/web/redis/sample/test/Redis4RedisTemplateTest.java
@@ -1,5 +1,9 @@
 package com.jingoal.web.redis.sample.test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.testng.annotations.Test;
@@ -18,8 +22,27 @@ public class Redis4RedisTemplateTest extends BaseTest {
 	}
 
 	@Test
-	public void ListOperationsTest(){
+	public void listOperationsTest(){
 		template.opsForList().leftPush("", "");
+	}
+	
+	@Test
+	public void hashOperationsTest(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("1", 1);
+		map.put("2", 2);
+		
+		template.boundHashOps("igoal.1.user.2").put("map.1", map);
+		template.boundHashOps("igoal.1.user.2").put("map.2", map);
+		template.boundHashOps("igoal.1.user.2").put("map.3", map);
+		template.boundHashOps("igoal.1.user.2").put("map.4", map);
+		
+		Set<Object> keys = template.boundHashOps("igoal.1.user.2").keys();
+		for (Object key : keys) {
+			System.out.println(key);
+		}
+		
+		//template.boundHashOps("igoal.1.user.2").delete("map.1", "map.2");
 	}
 	
 	public void myTest(){
